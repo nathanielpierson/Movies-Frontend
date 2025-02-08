2a868b9095c3b05bcaa7879f25410c8817566baf
nathanielpierson <nathanielepierson@gmail.com>
2025-02-08 15:45:27 -0500

    add modal

--- src/MoviesPage.jsx
+++ src/MoviesPage.jsx
@@ -3,8 +3,23 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 
 export function MoviesPage () {
-  const movies = 0;
-  const handleShow = 0;
+  const [movies, setMovies] = useState([]);
+  const [isMoviesShowVisible, setIsMovieShowVisible] = useState(false);
+  const [currentMovie, setCurrentMovie] = useState({});
+
+  const handleIndex = () => {
+    return (
+      axios.get('http://localhost:3000/movies.json')
+        .then(function (response) {
+        setMovies(response.data)
+        } )
+      )
+  }
+
+  const handleShow = (movie) => {
+  setCurrentMovie(movie);
+  setIsMovieShowVisible(true);
+  };
 return (
 <MoviesIndex movies={movies} onShow={handleShow} />
 )
