7c825ee9f42cbdc014f6b078583b9f6d18b6b641
nathanielpierson <nathanielepierson@gmail.com>
2025-03-25 20:26:38 -0400

    bug fixes

--- src/MoviesPage.jsx
+++ src/MoviesPage.jsx
@@ -1,84 +1,92 @@
-import { MoviesIndex } from './MoviesIndex';
-import { MoviesNew } from './MoviesNew';
-import { MoviesShow } from './MoviesShow';
-import { Modal } from './Modal';
-import axios from 'axios';
-import { useState, useEffect } from 'react'
+import { MoviesIndex } from "./MoviesIndex";
+// import { MoviesNew } from "./MoviesNew";
+import { MoviesShow } from "./MoviesShow";
+import { Modal } from "./Modal";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export function MoviesPage() {
-  const [recipes, setRecipes] = useState([]);
+  const [movies, setMovies] = useState([]);
   const [isMoviesShowVisible, setIsMovieShowVisible] = useState(false);
   const [currentMovie, setCurrentMovie] = useState({});
 
   const handleIndex = () => {
-    console.log('doing something...');
+    console.log("doing something...");
     // get data from rails
     // with a web request
     // Make a request for a user with a given ID
-    axios.get('http://localhost:3000/recipes.json')
-    .then(function (response) {
+    axios.get("http://localhost:3000/movies.json").then(function (response) {
       // handle success
-      console.log('inside the .then')
+      console.log("inside the .then");
       console.log(response.data);
-      setRecipes(response.data);
-    })
-    console.log('after the .then')   
-  }
+      setMovies(response.data);
+    });
+    console.log("after the .then");
+  };
 
   const handleShow = (movie) => {
     console.log(movie);
     setCurrentMovie(movie);
-    console.log('handling the show...');
-    setIsMoviesShowVisible(true)
-  }
+    console.log("handling the show...");
+    setIsMovieShowVisible(true);
+  };
 
   const closeModal = () => {
-    console.log('close the modal....');
-    setIsMoviesShowVisible(false)
-  }
+    console.log("close the modal....");
+    setIsMovieShowVisible(false);
+  };
 
-  const handleCreate = (params) => {
-    console.log('handle create...');
-    axios.post("http://localhost:3000/movies.json", params).then(response => {
-      console.log(response.data)
-      // make a copy of the origianl recipe array, add the new recipe to it
-      // reset recipes to the new array
-      setMovies([...movies, response.data])
-      // window.location.href = '/'
-    })
-  }
+  // const handleCreate = (params) => {
+  //   console.log('handle create...');
+  //   axios.post("http://localhost:3000/movies.json", params).then(response => {
+  //     console.log(response.data)
+  //     // make a copy of the origianl recipe array, add the new recipe to it
+  //     // reset movies to the new array
+  //     setMovies([...movies, response.data])
+  //     // window.location.href = '/'
+  //   })
+  // }
 
   const handleUpdate = (params, movie) => {
-    console.log('hanlding update')
-    axios.patch(`http://localhost:3000/movies/${movie.id}.json`, params).then(response => {
-      console.log(response.data)
-      setIsMoviesShowVisible(false)
-      // loop through recipes array
-      // find the recipe i want to update
-      // swap out the current values for response.data
-      setMovies(movies.map(r => r.id === response.data.id ? response.data : r))
-    })
-  }
+    console.log("hanlding update");
+    axios
+      .patch(`http://localhost:3000/movies/${movie.id}.json`, params)
+      .then((response) => {
+        console.log(response.data);
+        setIsMovieShowVisible(false);
+        // loop through movies array
+        // find the recipe i want to update
+        // swap out the current values for response.data
+        setMovies(
+          movies.map((r) => (r.id === response.data.id ? response.data : r))
+        );
+      });
+  };
 
   const handleDestroy = (movie) => {
-    console.log('handling detroy...')
-    axios.delete(`http://localhost:3000/movies/${movie.id}.json`).then(response => {
-      console.log(response.data);      
-      setMovies(recipes.filter(r => r.id !== movie.id))
-      setIsMoviesShowVisible(false)
-
-    })
-  }
+    console.log("handling detroy...");
+    axios
+      .delete(`http://localhost:3000/movies/${movie.id}.json`)
+      .then((response) => {
+        console.log(response.data);
+        setMovies(movies.filter((r) => r.id !== movie.id));
+        setIsMovieShowVisible(false);
+      });
+  };
 
   useEffect(handleIndex, []);
 
   return (
     <div>
       {/* <button onClick={handleIndex}>Get data from rails</button> */}
-      <MoviesNew onCreate={handleCreate} />
+      {/* <MoviesNew onCreate={handleCreate} /> */}
       <MoviesIndex movies={movies} onShow={handleShow} />
       <Modal show={isMoviesShowVisible} onClose={closeModal}>
-        <MoviesShow movie={currentMovie} onUpdate={handleUpdate} onDestroy={handleDestroy} />
+        <MoviesShow
+          movie={currentMovie}
+          onUpdate={handleUpdate}
+          onDestroy={handleDestroy}
+        />
       </Modal>
     </div>
   );
